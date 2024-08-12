20ed3e518d6ded49f3e1b7d6a9c6d6672aa83dec
Daniel Sada <2849006+danielsada@users.noreply.github.com>
2024-08-12 02:26:25 +0000

    fun playground

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Eya and Seif")
+if st.checkbox("Hello"):
+ if st.checkbox("everyone"):
+  if st.checkbox("I'm"):
+   if st.checkbox("eya"):
+    if st.checkbox("and"):
+     if st.checkbox("this"):
+      if st.checkbox("is"):
+       if st.checkbox("seif"):
+        if st.checkbox("we both"):
+         if st.checkbox("wanted"):
+          if st.checkbox("to"):
+           if st.checkbox("tell"):
+            if st.checkbox("you"):
+             if st.checkbox("something..."):
+              if st.checkbox("are"):
+               if st.checkbox("you.."):
+                if st.checkbox("ready?"):
+                 if st.checkbox("we..."):
+                  if st.checkbox("are..."):
+                   st.title("having a baby!")
+                   st.write("Don't tell anybody in the chat, just say congrats or nothing specific!")
+                   st.balloons()
+                   st.snow()
